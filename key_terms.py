8f5e7831ef78b3a380a703bd04d912b4c388f8a7
chrisbruh26 <50998508+chrisbruh26@users.noreply.github.com>
2025-04-27 19:05:58 -0700

    Create key_terms.py

---
+++ key_terms.py
@@ -0,0 +1,171 @@
+Amygdala: 
+
+Anterior:  
+
+anterior commissure:  
+
+arachnoid layer:  
+
+association cortex:  
+
+autonomic nervous system:
+
+basal ganglia:
+
+Brainstem:  
+
+Caudal:
+
+central canal:  
+
+central nervous system (CNS):  
+
+central sulcus:  
+
+Cerebellum:  
+
+cerebral aqueduct:  
+
+cerebral hemisphere:  
+
+cerebrospinal fluid (CSF):  
+
+Chordates:  
+
+cingulate cortex: 
+
+Contralateral:  
+
+coronal/frontal section:  
+
+corpus callosum:  
+
+cranial nerve:  
+
+Diencephalon:  
+
+Distal:  
+
+Dorsal:  
+
+dorsolateral prefrontal cortex (DLPC):  
+
+dura mater:  
+
+enteric nervous system:  
+
+Forebrain:  
+
+frontal lobe:  
+
+gray matter:  
+
+Gyrus/gyri:  
+
+Hindbrain:  
+
+Hippocampus:  
+
+Hominin:  
+
+Homo sapiens:  
+
+horizontal/axial/transverse section:  
+
+Hypothalamus:  
+
+Inferior:  
+
+Ipsilateral:  
+
+Lateral:  
+
+limbic system:  
+
+locus coeruleus:  
+
+Medial:  
+
+Medulla/myelencephalon:  
+
+Meninges: 
+
+Mesencephalon:  
+
+Metencephalon:  
+
+Midbrain:  
+
+Midline:  
+
+nucleus accumbens:  
+
+occipital lobe:  
+
+orbitofrontal cortex:  
+
+parasympathetic nervous system:  
+
+parietal lobe:  
+
+periaqueductal gray: 
+
+peripheral nervous system (PNS):  
+
+pia mater:  
+
+pituitary gland:  
+
+Pons:  
+
+postcentral gyrus:  
+
+Posterior:  
+
+precentral gyrus:  
+
+Proximal:  
+
+raphe nuclei:  
+
+Reflex:  
+
+reticular formation:  
+
+Rostral:  
+
+sagittal section:  
+
+somatic nervous system: 
+
+spinal cord: 
+
+subarachnoid space: 
+
+substantia nigra: 
+
+sulcus/sulci: 
+
+superior: 
+
+sympathetic nervous system: 
+
+telencephalon: 
+
+temporal lobe: 
+
+thalamus: 
+
+ventral: 
+
+ventricle: 
+
+white matter:
+
+CSF:
+
+hydrocephalus:
+
+neurons:
+
+ventromedial: 
